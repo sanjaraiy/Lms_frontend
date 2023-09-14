acf322d187539c045add92777c4b3d2797906a91
sanjaraiy <deepakrai78788@gmail.com>
2023-09-15 02:07:04 +0530

    add axios instance

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import './index.css';
 //Library imports
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
