68c715931d808bba5811812fae0047f0aae551fd
sanjaraiy <deepakrai78788@gmail.com>
2023-09-14 20:49:45 +0530

    added react hot toast

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
          <App />
+         <Toaster></Toaster>
     </BrowserRouter> 
     
  
