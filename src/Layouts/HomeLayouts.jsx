17645a5cf258499602344c4873566170be879cb1
sanjaraiy <deepakrai78788@gmail.com>
2024-01-24 15:39:22 +0530

    login and signup btns enable

--- src/Layouts/HomeLayouts.jsx
+++ src/Layouts/HomeLayouts.jsx
@@ -1,8 +1,19 @@
 import {FiMenu} from 'react-icons/fi';
 import { AiFillCloseCircle } from "react-icons/ai";
-import { Link } from 'react-router-dom';
+import { Link,useNavigate} from 'react-router-dom';
 import Footer from '../Components/Footer';
+import {useDispatch, useSelector} from 'react-redux'
+
 function HomeLayouts({children}) {
+     
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
+    
+    //for checking if user is logged in
+    const isLoggedIn = useSelector((state)=>state?.auth?.isLoggedIn);
+
+    //for displaying the options acc to role
+    const role=useSelector((state)=>state?.auth?.role);
 
     function changeWidth(){
         const drawerSide=document.getElementsByClassName("drawer-side");
@@ -40,6 +51,14 @@ function HomeLayouts({children}) {
                     <li>
                         <Link to="/">Home</Link>
                     </li>
+                    {
+                        isLoggedIn && role ==='ADMIN' && (
+                            <li>
+                                <Link to="/admin/dashboard">Admin DashBoard</Link>
+                            </li>
+                        )
+                    }
+
                     <li>
                         <Link to="/courses">All Courses</Link>
                     </li>
@@ -49,6 +68,18 @@ function HomeLayouts({children}) {
                     <li>
                         <Link to="/about">About Us</Link>
                     </li>
+                    {!isLoggedIn && (
+                        <li className='absolute bottom-4 w-[90%]'>
+                        <div className='w-full flex items-center justify-center'>
+                            <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
+                               <Link to="/login">Login</Link>
+                            </button>
+                            <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
+                               <Link to="/login">Sign up</Link>
+                            </button>
+                        </div>
+                        </li>
+                    )}
                 </ul>
 
             </div> 
