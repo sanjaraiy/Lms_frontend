60ededf9a3e64da32e0ee469e0686d345c1ca5f2
sanjaraiy <deepakrai78788@gmail.com>
2024-07-07 05:03:12 +0530

    added denied Page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Signup from './Pages/Signup';
 import Login from './Pages/Login'
 import CourseList from './Pages/Course/CourseList';
 import Contact from './Pages/Contact';
+import Denied from './Pages/Denied';
 
 
 
@@ -21,6 +22,7 @@ function App() {
           <Route path="/about" element={<AboutUs/>}></Route>
           <Route path="/signup" element={<Signup></Signup>}></Route>
           <Route path="/contact" element={<Contact></Contact>}></Route>
+          <Route path="/denied" element={<Denied></Denied>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
           <Route path="/courses" element={<CourseList></CourseList>}></Route>
           <Route path='*' element={<NotFound></NotFound>}></Route>
