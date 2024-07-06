80665eaedaa61a7aa063b69169a1659f36169b33
sanjaraiy <deepakrai78788@gmail.com>
2024-07-07 03:11:57 +0530

    added course page List

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Routes,Route } from 'react-router-dom'
 import NotFound from './Pages/NotFound';
 import Signup from './Pages/Signup';
 import Login from './Pages/Login'
+import CourseList from './Pages/Course/CourseList';
 function App() {
 
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/about" element={<AboutUs/>}></Route>
           <Route path="/signup" element={<Signup></Signup>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
+          <Route path="/courses" element={<CourseList></CourseList>}></Route>
           <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </>
---
+++ src/Pages/Course/CourseList.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCourses } from "../../Redux/Slices/CourseSlice";
+import HomeLayouts from "../../Layouts/HomeLayouts";
+import CourseCard from "../../Components/CourseCard";
+
+function CourseList(){
+  const dispatch = useDispatch();
+
+  const {courseData} = useSelector((state) => state.course);
+
+  async function loadCourses(){
+     await dispatch(getAllCourses());
+  }
+  useEffect(()=>{
+     loadCourses();
+  },[]);
+}
+
+return (
+    <HomeLayouts>
+        <div className="min-h-[90vh] pt-12 pl-20 flex flex-col gao-10 text-white">
+             <h1 className="text-center text-3xl font-semibold mb-5">
+                Explore the courses made by
+                <span className="font-bold text-yellow-500">
+                     Industry experts
+                </span>
+            </h1>
+            <div className="mb-10 flex flex-wrap gap-14">
+                {courseData?.map((ele) => {
+                     return <CourseCard key={ele._id} data={ele}></CourseCard>
+                })}
+            </div>
+             
+        </div>
+    </HomeLayouts>
+)
+
+export default CourseList;
